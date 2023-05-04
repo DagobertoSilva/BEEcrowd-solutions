b3944b9abcb89a1abc33297be129a8031ab58bb2
Dagoberto Silva <102236892+DagobertoSilva@users.noreply.github.com>
2023-05-04 19:11:08 -0300

    Update 1157.c

--- 1157.c
+++ 1157.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
  
 int main() {
- int N, d;
-scanf("%d", &N);
-for(int i=1; N>=i;i++){
-    d = N%i;
-    if(d==0){
-        printf("%d\n", i);
+ int N, d; //N é o numero que vou receber e d é a variável que vai armazenar os restos
+scanf("%d", &N); //lendo o número digitado 
+for(int i=1; N>=i;i++){ //contador vai ate o numero que digitei, sendo somado mais 1
+    d = N%i; // restos das divisões
+    if(d==0){ // estou vendo onde o numero é divisivel, pois se o resto dor 0 o numero é divisivel por aquele numero;
+        printf("%d\n", i);// aqui imprimo os divisores do número digitado
     }
 }
  
