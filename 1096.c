e64d763ed4750c298cda54d557b6663b90909833
Dagoberto Silva <102236892+DagobertoSilva@users.noreply.github.com>
2023-05-04 18:35:51 -0300

    Update 1096.c

--- 1096.c
+++ 1096.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 int main(){
-int I = 1;
-do{
+int I = 1; //atribuindo valor para I
+do{    //repetição para fazer que o I e o J se repita 
  for(int J =7; J>=5; J--){
     printf("I=%d J=%d\n", I+=0, J);
  }
- I+=2;
-}while(I!=11);
+ I+=2; //após o j chegar em 5, vou somar dois ao valor de I repetir o laço. 
+}while(I!=11); //tudo vai se repetir até o I chegar em 9
 }
