4b6cc177ed223a905bbc1b5d3326f1fc57f063a6
Dagoberto Silva <102236892+DagobertoSilva@users.noreply.github.com>
2023-05-05 14:50:41 -0300

    answer

---
+++ 1003.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+ 
+int main() {
+ 
+    int A, B; //declarando as variáveis
+scanf("%d %d", &A, &B); //recebenso valores do usuário
+printf("SOMA = %d\n", A+B); // somando e imprimindo a soma
+ 
+    return 0;
+}
