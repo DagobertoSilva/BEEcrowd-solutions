234330b5b9cff938dc28d638999f94fc82c918a8
Dagoberto Silva <102236892+DagobertoSilva@users.noreply.github.com>
2023-05-05 14:56:13 -0300

    answer

---
+++ 1004.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+ 
+int main() {
+ 
+int A, B; //declarando as variáveis
+scanf("%d %d", &A, &B); //recebenso valores do usuário
+printf("PROD = %d\n", A*B); // FAZENDO O PRODUTO e imprimindo O PRODUTO
+ 
+    return 0;
+}
