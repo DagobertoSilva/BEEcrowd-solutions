c9030cc3f28d4f0a8bf169baef6b1642ca12ef7c
Dagoberto Silva <102236892+DagobertoSilva@users.noreply.github.com>
2023-05-05 18:32:59 -0300

    Area

---
+++ 1012.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+ 
+int main() {
+float A, B, C, pi = 3.14159;
+scanf("%f %f %f", &A, &B, &C);  //recebendo as variáveis do usuário
+printf("TRIANGULO: %.3f\n", (A*C)/2);
+printf("CIRCULO: %.3f\n", 3.14159*(C*C));
+printf("TRAPEZIO: %.3f\n", (A+B)*C/2);      //fazendo o calculo de cada area com os valores digitados e os exibindo 
+printf("QUADRADO: %.3f\n", B*B);
+printf("RETANGULO: %.3f\n", (A*B));
+    return 0;
+}
