d91240c74dc156605374108be60ce78b419f8e13
Dagoberto Silva <102236892+DagobertoSilva@users.noreply.github.com>
2023-08-11 04:16:24 -0300

    1001 - Extremely Basic

---
+++ 1001.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int main() {
+ int a, b, X; // iniciando as variaveis
+ scanf("%d %d", &a,&b); //recebendo as variáveis
+ X = a+b; // fazendo a soma das variávei e colocando no X
+ printf("X = %d", X); // imprimindo a soma
+}
